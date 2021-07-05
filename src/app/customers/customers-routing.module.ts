087b81ab088359443a5ad31c227f8041d08ae54f
Daniel Kreider <daniel@danielk.tech>
2021-07-05 18:12:51 -0300

    Add customers module

---
+++ src/app/customers/customers-routing.module.ts
@@ -0,0 +1,16 @@
+import { CustomersComponent } from './customers.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CustomersComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CustomersRoutingModule { }
