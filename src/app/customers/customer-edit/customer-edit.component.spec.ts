e03012bc2489f80c86dae3ee4ec9bb67922f44ee
Daniel Kreider <daniel@danielk.tech>
2021-07-06 17:49:38 -0300

    Fixing broken tests

--- src/app/customers/customer-edit/customer-edit.component.spec.ts
+++ src/app/customers/customer-edit/customer-edit.component.spec.ts
@@ -18,8 +18,4 @@ describe('CustomerEditComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/customers/customers.component.spec.ts
+++ src/app/customers/customers.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { createSpyFromClass } from 'jasmine-auto-spies';
+import { CustomersService } from '../shared/services/customers.service';
 
 import { CustomersComponent } from './customers.component';
 
@@ -8,7 +10,10 @@ describe('CustomersComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CustomersComponent ]
+      declarations: [ CustomersComponent ],
+      providers: [
+        { provide: CustomersService, useValue: createSpyFromClass(CustomersService)}
+      ]
     })
     .compileComponents();
   });
